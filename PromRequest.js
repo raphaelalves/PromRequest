75aa596a31552d32199d2e18529c3b8d73db9c2b
Raphael Alves Da Silva <cin_rsilva@uolinc.com>
2017-05-12 09:04:48 -0300

    Add more commentaries

--- PromRequest.js
+++ PromRequest.js
@@ -16,13 +16,19 @@ function removeProtocolFromURL(url) {
 }
 
 function doRequest(requestSettings, pathname) {
+	// Define which library will be used to send the request
 	const httpLibrary = isHttps(requestSettings.hostname) ? require('https') : require('http');
+	// Remove the URL protocol
 	requestSettings.hostname = removeProtocolFromURL(requestSettings.hostname);
+	// Add a path to be used along the main url
 	requestSettings.path = pathname;
+
 	return new Promise((resolve, reject) => {
 		httpLibrary.request(requestSettings, (res) => {
 			let responseData = [];
+			// Determine which encoding will be used on the request
 			requestSettings.responseEncode ? res.setEncoding(requestSettings.responseEncode) : res.setEncoding('utf8');
+			// Push each chunk of the data inside the array
 			res.on('data', (requestData) => responseData.push(requestData));
 			res.on('error', (requestError) => reject(requestError));
 			res.on('end', () => {
@@ -37,12 +43,13 @@ function doRequest(requestSettings, pathname) {
 }
 
 function promRequest(url, options) {
-	options = Object.assign({}, options, {hostname: url});
+	options = Object.assign({}, options, { hostname: url });
 	const settings = addRequestOptionsToDefault(options);
+	// Return the functions already binding the request settings
 	return {
-		get: doRequest.bind(this, settings),
-		post: doRequest.bind(this, settings),
-		put: doRequest.bind(this, settings),
+		get:    doRequest.bind(this, settings),
+		post:   doRequest.bind(this, settings),
+		put:    doRequest.bind(this, settings),
 		delete: doRequest.bind(this, settings),
 	};
 }
